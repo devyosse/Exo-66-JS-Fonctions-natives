72838ef1af5c72e1fa10cfc4609b3a24ed27b4ad
devyosse <narutoshippuden884@gmail.com>
2021-11-16 16:24:44 +0100

    exo 66

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log(random());
+let operande = 10;
+
+let paraOne = document.getElementById('entier');
+paraOne.innerHTML = parseInt(paraOne.innerHTML) * operande;
+
+let paraTwo = document.getElementById('flottant');
+paraTwo.innerHTML = parseFloat(paraTwo.innerHTML) * operande + parseInt(paraOne.innerHTML);
+
+let paraThree = document.getElementById('non-numerique');
+
+if (isNaN(paraThree)){
+    paraThree.innerHTML = '0';
+}
+
+let paraFour= document.getElementById('random');
+
+paraFour.innerHTML = Math.random();
+
+let paraFive = document.getElementById('trunc');
+
+paraFive.innerHTML =  Math.trunc(54);
+
+
+let paraSix = document.getElementById('replace');
+
+paraSix.innerHTML= paraSix.innerHTML.replace(' world ', ' Hocine Hassaini')
+
+let random = document.getElementById('random');
+random.innerHTML = Math.trunc(Math.random() *100);
+console.log(random);
